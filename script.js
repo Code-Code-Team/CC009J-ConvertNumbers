14c687fb0d5b95b73056d65d54db2d267be06403
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-04 13:43:30 +0300

    input check and alert added acording to regex

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const binaryInputElement = document.getElementById('binary')
+const binaryInputElement = document.getElementById('binary');
 
-const decInputElement = document.getElementById('dec')
+const decInputElement = document.getElementById('dec');
 
+const buttonElement = document.querySelector('button');
 
-const buttonElement = document.querySelector('button')
+binaryInputElement.addEventListener('keypress', (e) => {
+	!/^[0-1]{1,8}$/i.test(e.key) && alert('not a binary');
+});
 
+decInputElement.addEventListener('keyup', (e) => {
+	!/^[0-9]$/i.test(e.key) && alert('number only');
+});
 
-
-function bin2dec(val){  
-    const result = (parseInt(val,2))
-    decInputElement.value =  result
+function bin2dec(val) {
+	const result = parseInt(val, 2);
+	decInputElement.value = result;
 }
-function dec2bin(val){ 
-    const result = (Number(val).toString(2))
-    binaryInputElement.value = result
+function dec2bin(val) {
+	const result = Number(val).toString(2);
+	binaryInputElement.value = result;
 }
-buttonElement.addEventListener('click', ()=>{   
-    const valBinary = binaryInputElement.value
-    const valDec = decInputElement.value
-    if (valBinary & valDec){
-        console.log("input one pls")
-    }else{
-        valBinary ? bin2dec(valBinary) : dec2bin(valDec)
-    }
-})
+
+buttonElement.addEventListener('click', () => {
+	const valBinary = binaryInputElement.value;
+	const valDec = decInputElement.value;
+	if (valBinary & valDec) {
+		alert('clear one of input value');
+	} else {
+		valBinary ? bin2dec(valBinary) : dec2bin(valDec);
+	}
+});
